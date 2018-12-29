2818450df6f4d7be571a5277be34cca5ff20d929
WANG WEIQIANG <wangwq05@163.com>
2018-12-29 08:24:55 +0800

    Updates

---
+++ precache-manifest.d81c498bbd4ac58fd84de2fdf8ba8c5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/search-countries/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5b9212849b875661422a",
+    "url": "/search-countries/static/js/runtime~main.5b921284.js"
+  },
+  {
+    "revision": "c2a78db7297fa7954baa",
+    "url": "/search-countries/static/js/main.c2a78db7.chunk.js"
+  },
+  {
+    "revision": "74641edb98c3afc143a0",
+    "url": "/search-countries/static/js/1.74641edb.chunk.js"
+  },
+  {
+    "revision": "c2a78db7297fa7954baa",
+    "url": "/search-countries/static/css/main.a540590c.chunk.css"
+  },
+  {
+    "revision": "74641edb98c3afc143a0",
+    "url": "/search-countries/static/css/1.21a45ed9.chunk.css"
+  },
+  {
+    "revision": "9fe3664b49acdbdbbfe1e7a448287a15",
+    "url": "/search-countries/index.html"
+  }
+];
